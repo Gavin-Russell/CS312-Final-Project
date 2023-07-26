44f6f058cf1b610ddf21827eac4163fd03180a54
Gavin Russell <93464986+Gavin-Russell@users.noreply.github.com>
2023-07-25 20:32:12 -0700

    added prototype react app
    Added the react app for the login/signup/cookies prototype for phase 1

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from './MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
