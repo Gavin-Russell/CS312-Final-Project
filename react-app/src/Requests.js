c96ac64b98f73ce3dd5d1b35533300fc7e7bf32b
Jacob Olson <jdo227@nau.edu>
2023-07-27 17:36:11 -0700

    handle different status codes in frontend api

--- react-app/src/Requests.js
+++ react-app/src/Requests.js
@@ -1,11 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-export default function LogIn(user) {
-    return axios.post('http://localhost:3001/logIn', user)
+export default async function LogIn(user) {
+  const response = await axios.post("http://localhost:3001/logIn", user);
+  if (response.status == 200) {
+    console.log("successful login");
+    return response;
+  }
+  if (response.status == 400) {
+    console.log("400 status, login unsuccessful");
+    return null;
+  }
+  console.log(`something weird happened, status: ${response.status}`);
+  return null;
 }
 
-
 export function SignUp(newUser) {
-    return axios.post('http://localhost:3001/signUp', newUser)
+  return axios.post("http://localhost:3001/signUp", newUser);
 }
