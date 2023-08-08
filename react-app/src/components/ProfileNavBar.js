44e0d84e1bba3355c04c5b07499a8c55bb2a32f2
ind9-nau <93492981+ind9-nau@users.noreply.github.com>
2023-08-08 12:43:21 -0700

    Removed comments history

--- react-app/src/components/ProfileNavBar.js
+++ react-app/src/components/ProfileNavBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import EditProfilePage from './EditProfilePage.js'
-import EditCommentsPage from './EditCommentsPage'
 import BlogPostList from './BlogPostList'
 import NewBlogPost from './NewBlogPost'
 import { deleteComment } from './Requests'
@@ -10,11 +9,6 @@ export class ProfileNavBar extends Component {
     handleEditProfile = (event) => {
         event.preventDefault()
         this.props.updatePageContent(<EditProfilePage username={this.props.username}></EditProfilePage>)
-    }
-
-    handleEditComments = (event) => {
-        event.preventDefault()
-        this.props.updatePageContent(<EditCommentsPage username={this.props.username} deleteHandler={this.deleteHandler}></EditCommentsPage>)
     }
 
     handleBlogPosts = (event) => {
@@ -40,7 +34,6 @@ export class ProfileNavBar extends Component {
         <div>
             <h1>Profile</h1>
             <button onClick={this.handleEditProfile}>Edit Profile</button>
-            <button onClick={this.handleEditComments}>Comment History</button>
             <button onClick={this.handleBlogPosts}>Blog Post History</button>
             <button onClick={this.handleNewBlogPost}>New Blog Post</button>
         </div>
